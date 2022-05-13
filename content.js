314c6424076f09470213ac9288dd869714864c8c
jamespilcher <pilcherjames0@gmail.com>
2022-05-13 17:19:50 +0100

    commit

--- content.js
+++ content.js
@@ -15,49 +15,38 @@ function shuffleWord(word){
 function wordReplacer(text,word){
     shuffledWord = shuffleWord(word);
     console.log(shuffledWord);
-
     text = text.replace(word, shuffledWord);
     return text;
 }
 
-function isValidWordList(wordList){
-    return true;
-}
-
 function anagramifyNode(text){
-    var anagramed_text = text;
+    var anagrammed_text = text;
     var wordList = text.match(/\b(\w+)\b/g);
-
-    if ( (wordList != null) && isValidWordList(wordList) ){
+    if ( (wordList != null) ){
         for (var i = 0; i < wordList.length; i++) {
             var word = wordList[i];
             console.log(word);
-            anagramed_text = wordReplacer(anagramed_text, word);
-            console.log(anagramed_text);
+            anagrammed_text = wordReplacer(anagrammed_text, word);
         }
     }
-    return anagramed_text;
+    return anagrammed_text;
 }
 
 function anagramifyPage(){
     var elements = document.getElementsByTagName('*');
+    var ignoreTags = ['script', 'style'];
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
-        var ignoreTags = ['script', 'style'];
         if (ignoreTags.includes(element.tagName.toLowerCase())) {
-            console.log("skipped");
             continue;
         }
         for (var j = 0; j < element.childNodes.length; j++) {
             var node = element.childNodes[j];
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
-
                 var replacedText = anagramifyNode(text);
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
-
-        
     }
 }
